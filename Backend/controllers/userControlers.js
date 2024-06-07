829dc4a99c0de9c1c6566fa66ba858f7d452bca6
Ratnesh <pillairatnesh96@gmail.com>
2024-06-07 12:09:35 +0530

    first commit

---
+++ Backend/controllers/userControlers.js
@@ -0,0 +1,55 @@
+const User = require("../models/userSchema");
+
+const createUser = async (req, res) => {
+  const newUser = new User(req.body);
+  try {
+    await newUser.save();
+    res.send({
+      status: 200,
+      message: "new user created",
+    });
+  } catch (error) {
+    res.send({
+      status: 500,
+      error: error,
+      message: "could not create new user",
+    });
+  }
+};
+const getAllUsers = async (req, res) => {
+  const allUsers = await User.find();
+  try {
+    res.send({
+      status: 200,
+      users: allUsers,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedUser = await User.findByIdAndDelete(id);
+    res.send({
+      status: 200,
+      deletedUser: deletedUser,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const updateUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updateUser = await User.findByIdAndUpdate(id, { $set: req.body });
+    res.send({
+      status: 200,
+      updatedUser: updateUser,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { createUser, getAllUsers, deleteUser, updateUser };
---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const userController = require("../controllers/userControlers");
+
+const router = express.Router();
+
+console.log(userController);
+router.post("/createuser", userController.createUser);
+router.get("/getAllusers", userController.getAllUsers);
+router.delete("/deleteUser/:id", userController.deleteUser);
+router.delete("/deleteUser/:id", userController.deleteUser);
+router.put("/updateUser/:id", userController.updateUser);
+
+module.exports = router;
